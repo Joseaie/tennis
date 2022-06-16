a6e9284c93593fc50c1167be3e136438073189f5
Josephine Obeng <64155953+Joseaie@users.noreply.github.com>
2022-06-16 14:32:42 +0100

    Refactor score loop

--- tennis_scorecard.rb
+++ tennis_scorecard.rb
@@ -1,17 +1,16 @@
 def score tennis_score
-    result = tennis_score.map{ |score| 
-        if score == 0
-            'love'
-        elsif score == 1
-            'fifteen'
-        elsif score == 2
-            'thirty'
-        elsif score == 3
-            'forty'
-        else
-            score
-        end
+    scores = {
+        0 => 'love',
+        1 => 'fifteen',
+        2 => 'thirty',
+        3 => 'forty',
+        4 => 'advantage'
     }
+
+    result = tennis_score.map{ |score| 
+        scores[score]
+    }
+        end
 
     return result
 end
